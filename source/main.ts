04fdf2f7fa79004cbf49abb4ea4304a41e31c852
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2022-01-28 22:47:16 +0100

    element generating function

--- source/main.ts
+++ source/main.ts
@@ -1,1 +1,32 @@
-const listElement = [];
+const reset = document.querySelector("#reset");
+const main = document.querySelector(".main");
+
+function createElements(){
+    const listElement = [];
+    main.innerHTML = "";
+    for (let i = 1; i<=10; i++){
+        const div = document.createElement("div");
+        div.innerHTML = `${i}`;
+        div.classList.add("bar", `bar_${i}`);
+        
+        const element = [];
+        element.push(i);
+        element.push(div)
+
+        listElement.push(element);
+    }
+    
+    return listElement;
+}
+
+function printElements(listElement) {
+    for (let i = 0; i<=9; i++){
+        main.appendChild(listElement[i][1]);
+    }
+}
+
+let list_element = createElements();
+
+reset.addEventListener("click", function(event){
+    printElements(createElements());
+});
